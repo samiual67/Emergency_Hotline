ec06346dd00b5552abdbaacc6fcb10ce0b283e60
samiual7 <mdsamiualmia67@gmail.com>
2025-08-29 14:45:27 +0600

    update code

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+let heartCount = 0;
+let copyCount = 0;
+let coinCount = 100; 
+const historyList = document.getElementById("historyList");
+
+function increaseHeart() {
+  heartCount++;
+  document.getElementById("heartCount").innerText = heartCount;
+
+  const heartIcon = event.target;
+  heartIcon.classList.add("text-red-500");
+}
+ 
+function copyNumber(number) {
+  navigator.clipboard.writeText(number).then(() => {
+    copyCount++;
+    document.getElementById("copyCount").innerText = copyCount;
+  
+    alert("Number copied: " + number);
+  });
+}
+
+function makeCall(serviceName, number) {
+  if (coinCount <= 0) {
+    alert("No coins left to make a call!");
+    return;
+  }
+
+  coinCount--;
+  document.getElementById("coinCount").innerText = coinCount;
+
+  const li = document.createElement("li");
+  li.textContent = `${serviceName} (${number}) - Called at ${new Date().toLocaleTimeString()}`;
+  historyList.prepend(li);
+}
+function clearHistory() {
+  historyList.innerHTML = "";
+}
