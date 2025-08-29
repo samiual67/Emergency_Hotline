7158245df568f49f493786e8b26f89bff3304031
samiual7 <mdsamiualmia67@gmail.com>
2025-08-29 20:11:19 +0600

    push text in readme.md file and pop some text from readme.md

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 
 
+// let heartCount = 0;
+// let copyCount = 0;
+// let coinCount = 100; 
+// const historyList = document.getElementById("historyList");
+
+// function increaseHeart() {
+//   heartCount++;
+//   document.getElementById("heartCount").innerText = heartCount;
+
+//   const heartIcon = event.target;
+//   heartIcon.classList.add("text-red-500");
+// }
+ 
+// function copyNumber(number) {
+//   navigator.clipboard.writeText(number).then(() => {
+//     copyCount++;
+//     document.getElementById("copyCount").innerText = copyCount;
+  
+//     alert("Number copied: " + number);
+//   });
+// }
+
+// function makeCall(serviceName, number) {
+//   if (coinCount <= 0) {
+//     alert("No coins left to make a call!");
+//     return;
+//   }
+
+//   coinCount--;
+//   document.getElementById("coinCount").innerText = coinCount;
+
+//   const li = document.createElement("li");
+//   li.textContent = `${serviceName} (${number}) - Called at ${new Date().toLocaleTimeString()}`;
+//   historyList.prepend(li);
+// }
+// function clearHistory() {
+//   historyList.innerHTML = "";
+// }
+
+
 let heartCount = 0;
 let copyCount = 0;
 let coinCount = 100; 
@@ -12,29 +52,31 @@ function increaseHeart() {
   const heartIcon = event.target;
   heartIcon.classList.add("text-red-500");
 }
- 
+
 function copyNumber(number) {
   navigator.clipboard.writeText(number).then(() => {
     copyCount++;
     document.getElementById("copyCount").innerText = copyCount;
-  
     alert("Number copied: " + number);
   });
 }
 
 function makeCall(serviceName, number) {
-  if (coinCount <= 0) {
-    alert("No coins left to make a call!");
+  if (coinCount < 20) {
+    alert("Not enough coins! Each call costs 20 coins.");
     return;
   }
 
-  coinCount--;
+  coinCount -= 20;  // Deduct 20 coins
   document.getElementById("coinCount").innerText = coinCount;
+
+  alert(`Calling ${serviceName} at ${number}...`);
 
   const li = document.createElement("li");
   li.textContent = `${serviceName} (${number}) - Called at ${new Date().toLocaleTimeString()}`;
   historyList.prepend(li);
 }
+
 function clearHistory() {
   historyList.innerHTML = "";
 }
